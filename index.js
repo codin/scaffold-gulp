3b4586c39c503c20e06bd548ff5ef9bd038be25b
Craig Childs <childscraig17@gmail.com>
2017-01-07 23:52:38 +0000

    Setup entrypoint, add package.json

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Scaffold gulp build system. Includes API to configure
+ * your Scaffold install for a number of modern frontend
+ * techniques.
+ */
+
+var defaultOptions = {};
+
+function Scaffold(options) {
+    this.gulp = require('gulp');
+    this.options = options.
+}
+
+module.exports = Scaffold;
