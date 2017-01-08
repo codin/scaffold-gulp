e6d950f8ab119b4d50d0d74cd4c6fe2302d54b47
Craig Childs <childscraig17@gmail.com>
2017-01-08 10:15:13 +0000

    Chain

--- src/styles-task.js
+++ src/styles-task.js
@@ -7,11 +7,15 @@ StylesTask.name = 'styles';
 StylesTask.prototype.scss = function() {
     this.options.language = 'scss';
     // Add gulp-sass to stream
+    
+    return this;
 };
 
 StylesTask.prototype.minify = function() {
     this.options.minify = true;
     // Add gulp-compact to stream
+    
+    return this;
 };
 
 module.exports = StylesTask;
